68496b1584c6c23c505693650a9b8c13fd0c8622
Iarasilval <loure.iara.s@gmail.com>
2023-09-16 13:59:08 -0300

    ap10(repeticao): codigo 2.4.7

--- estrutura_de_repeticao.sql
+++ estrutura_de_repeticao.sql
@@ -1,20 +1,71 @@
---CONTINUE
 DO $$
 DECLARE
+	nota INT;
+	media NUMERIC(10, 2) := 0;
 	contador INT := 0;
 BEGIN
-	LOOP
+	SELECT valor_aleatorio_entre(0, 11) -1 INTO nota;
+	WHILE nota >= 0 LOOP
+		RAISE NOTICE '%', nota;
+		media := media + nota; --acumulador 
 		contador := contador + 1;
-		EXIT WHEN contador > 100;
-		-- ignorando as iterações em que multiplo de 7 
-		IF contador % 7 = 0 THEN 
-			CONTINUE;
-		END IF;
-		CONTINUE WHEN contador % 11 = 0; 
-		RAISE NOTICE '%', contador;
+		SELECT valor_aleatorio_entre(0, 11) -1 INTO nota;
 	END LOOP;
+	IF contador > 0 THEN -- se tem pelo menos uma nota 
+		RAISE NOTICE 'MEdia: %', media/ contador;
+	ELSE 
+		RAISE NOTICE 'Nenhuma nota gerada';
+	END IF;
 END;
 $$
+
+-- DO $$
+-- DECLARE 
+-- 	i INT;
+-- 	j INT;
+-- BEGIN
+-- 	i := 0;
+-- 	-- esse é um rotulo 
+-- 	<<externo>>
+-- 	LOOP 
+-- 		i := i + 1;
+-- 		EXIT WHEN i > 10;
+-- 		j :=  1;
+-- 		<<interno>>
+-- 		LOOP 
+-- 			RAISE NOTICE '%, %', i, j;
+-- 			j := j + 1;
+-- 			EXIT WHEN j > 10;
+-- 			EXIT externo WHEN j > 5;
+-- 		END LOOP;
+-- 	END LOOP;
+-- END;
+-- $$
+-- 1 6 Não 
+--1 5 Sim
+--1 4 Sim
+-- 1 3 Sim
+--1 2 Sim
+-- 0 1 Não aparece 
+-- 1 1 Sim aparece
+
+--CONTINUE
+-- DO $$
+-- DECLARE
+-- 	contador INT := 0;
+-- BEGIN
+-- 	LOOP
+-- 		contador := contador + 1;
+-- 		EXIT WHEN contador > 100;
+-- 		-- ignorando as iterações em que multiplo de 7 
+-- 		IF contador % 7 = 0 THEN 
+-- 			CONTINUE;
+-- 		END IF;
+-- 		CONTINUE WHEN contador % 11 = 0; 
+-- 		RAISE NOTICE '%', contador;
+-- 	END LOOP;
+-- END;
+-- $$
 
 
 -- --EXIT WHEN
